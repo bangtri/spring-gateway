6bb58a3550fd85a8368b65add26fe7eede2d668a
bangtri <tribang.soict.hust@gmail.com>
2022-11-29 08:27:06 +0700

    push spring-gateway

---
+++ src/main/java/bang/nt/spring/gateway/config/GatewayFilter.java
@@ -0,0 +1,33 @@
+package bang.nt.spring.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.Objects;
+import java.util.Set;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.*;
+
+@Slf4j
+@Component
+public class GatewayFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        Set<URI> originalUris = exchange.getAttribute(GATEWAY_ORIGINAL_REQUEST_URL_ATTR);
+        if (Objects.nonNull(originalUris)) {
+            URI originalUri = originalUris.iterator().next();
+            Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+            URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+            if (Objects.nonNull(route)) {
+                log.info("Incoming request " + originalUri.toString() + " is routed to id: " + route.getId() + ", uri:" + routeUri);
+            }
+        }
+        return chain.filter(exchange);
+    }
+}
